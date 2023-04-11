37e7057f5c80f18e034fb5dca501e63920247605
Angiefiona <angellamumoh@gmail.com>
2023-04-11 09:07:45 +0300

    first commit

---
+++ assesment.js
@@ -0,0 +1,57 @@
+//1. Write a function that takes in a string and returns it when reversed
+//let food = “eating”
+let food = "eating"
+let reverse= food.reverse("eating");
+console.log(reverse)
+
+//Write a function that takes in the following array and consoles the target if it is found else
+//null
+//let num = [2,8,0,23,5,45,76]
+//Target = 23
+
+function binary(num,target){
+    let left = 0;
+    let middle = Math.floor(left+right/2);
+    if(num[middle]<=target){
+        return middle
+        while(num[middle]<=left.length){
+         return left
+        }
+        if(num[middle]<=right.length -1){
+
+        }
+    }
+    return null
+}
+target = 23
+let num = [2,8,0,23,5,45,76]
+console.log(binary(num,target))
+
+//3. Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+//“Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+//let numbers = 
+if(numbers 0..100 % 15){
+return FizzBuzz
+else(numbers 0..100 % 5){
+ return Buzz
+}
+if else(numbers 0..100 % 3);
+  return Fizz
+}
+
+//4. Write a function that takes in an array of numbers and returns an array that has all
+//elements multiplied by 4.
+//let numArray = [12,87,45,75,23,64,73]
+
+let numArray = [12,87,45,75,23,64,73];
+let product = numArray*4
+console.log(product)
+
+//5. Write a function that takes in an array of strings and returns an array with every element
+//turned into a number
+//let nums = [“10”,”24”,”45”,”56”,”67”]
+
+let nums = ["10","24", "45","56", "67"];
+let strings= nums.unshift()
+console.log(strings)
+
